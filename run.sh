0cf5c2342c21308fe855b68a8f435a668947a664
wcy_scnu_nlp <91064816+swaggy66@users.noreply.github.com>
2025-09-22 12:45:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ for iter in 1 2 3 4 5 6 7 8 9 10
 do
 echo --- $iter ---
 python -u train.py --lr 0.0001 --batch-size 16 --epochs 150 --temp 1 --Dataset 'IEMOCAP'
-done > sdt_iemocap.txt 2>&1 &
+done > sure_iemocap.txt 2>&1 &
